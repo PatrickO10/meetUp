c0e200d3af2f9af3c46990466c631305c73b2fc0
patricko10 <patrickorth10@gmail.com>
2015-12-14 18:49:29 -0600

    adds build tools

---
+++ dist/js/all.js
@@ -0,0 +1,9 @@
+(function () {
+	var foo = 123;
+	return foo;
+})();
+
+
+var timez = 'this is the police speaking';
+
+alert(timez);
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
+
+gulp.task('default', ['copy-html', 'styles', 'lint', 'scripts'], function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('js/**/*.js', ['lint']);
+    gulp.watch('index.html', ['copy-html']);
+    gulp.watch('./dist/index.html').on('change', browserSync.reload);
+    browserSync.init({
+        server: "./dist"
+    });
+
+});
+
+gulp.task('dist', [
+    'copy-html',
+    'copy-images',
+    'styles',
+    'lint',
+    'scripts-dist'
+]);
+
+gulp.task('scripts', function() {
+    gulp.src('js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scripts-dist', function() {
+    gulp.src('js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-html', function() {
+    gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function() {
+    gulp.src('img/*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('lint', function() {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['js/**/*.js', '!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
