764e277295fb9bf09bc2bafe1014329f2f529523
patricko10 <patrickorth10@gmail.com>
2016-01-11 02:50:06 -0600

    adds creating a new user and authenticating user to firebase

--- js/app.js
+++ js/app.js
@@ -1,103 +1,44 @@
 var submitSignUp = document.getElementById('submitSignUp'),
-    submitLogin = document.getElementById('submitLogin'),
-    logPass = document.getElementById('logPass'),
-    logEmail = document.getElementById('logEmail'),
-    firstPasswordEl = document.getElementById('fPassword'),
-    secondPasswordEl = document.getElementById('sPassword'),
-    autocomplete;
+	submitLogin = document.getElementById('submitLogin'),
+	logPass = document.getElementById('logPass'),
+	logEmail = document.getElementById('logEmail'),
+	firstPasswordEl = document.getElementById('fPassword'),
+	secondPasswordEl = document.getElementById('sPassword'),
+	autocomplete;
 
 function initAutocomplete() {
-    var input = document.getElementById('loc-input');
-    // Create the autocomplete object
-    autocomplete = new google.maps.places.Autocomplete(input);
+	var input = document.getElementById('loc-input');
+	// Create the autocomplete object
+	autocomplete = new google.maps.places.Autocomplete(input);
 }
 
 // This will track issues.
-function trackIssues() {
-    this.issues = [];
+function TrackIssues() {
+	this.issues = [];
 }
 
-trackIssues.prototype = {
-    // Method to add issues to the this.issues array.
-    addIssues: function(issue) {
-        this.issues.push(issue);
-    },
-    // Method to retrieve issues.
-    retrieveIssues: function() {
-        var msg = "";
-        if (this.issues.length === 1) {
-            msg = "The following issue needs to be corrected:\n" + this.issues[0];
-        } else if (this.issues.length > 1) {
-            msg = "The following issues need to be corrected:\n" + this.issues.join("\n");
-        }
-        return msg;
-    }
+TrackIssues.prototype = {
+	// Method to add issues to the this.issues array.
+	addIssues: function(issue) {
+		this.issues.push(issue);
+	},
+	// Method to retrieve issues.
+	retrieveIssues: function() {
+		var msg = "";
+		if (this.issues.length === 1) {
+			msg = "The following issue needs to be corrected:\n" + this.issues[0];
+		} else if (this.issues.length > 1) {
+			msg = "The following issues need to be corrected:\n" + this.issues.join("\n");
+		}
+		return msg;
+	}
 };
 
-submitSignUp.onclick = function() {
-    var firstPasswordVal = firstPasswordEl.value,
-        secondPasswordVal = secondPasswordEl.value;
-    /*
-     * Make an issue tracker for each password input to have two separate issue trackers.
-     */
-    var firstPasswordInputIssuesTracker = new trackIssues(),
-        secondPasswordInputIssuesTracker = new trackIssues();
-
-    /*
-     * Checks to make sure first password doesn't have any issues.
-     */
-    function checkPasswords() {
-        if (firstPasswordVal.length < 8) {
-            firstPasswordInputIssuesTracker.addIssues("fewer than 8 characters");
-        } else if (firstPasswordVal.length > 50) {
-            firstPasswordInputIssuesTracker.addIssues("greater than 50 characters");
-        }
-
-        if (!firstPasswordVal.match(/[\!\@\#\$\%\^\&\*]/g)) {
-            firstPasswordInputIssuesTracker.addIssues("missing a symbol (!, @, #, $, %, ^, &, *)");
-        }
-
-        if (!firstPasswordVal.match(/\d/g)) {
-            firstPasswordInputIssuesTracker.addIssues("missing a number");
-        }
-
-        if (!firstPasswordVal.match(/[a-z]/g)) {
-            firstPasswordInputIssuesTracker.addIssues("missing a lowercase letter");
-        }
-
-        if (!firstPasswordVal.match(/[A-Z]/g)) {
-            firstPasswordInputIssuesTracker.addIssues("missing an uppercase letter");
-        }
-
-        var badCharacterGroup = firstPasswordVal.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g);
-        if (badCharacterGroup) {
-            badCharacterGroup.forEach(function(badChar) {
-                firstPasswordInputIssuesTracker.addIssues("includes bad character: " + badChar);
-            });
-        }
-    }
-
-    if (firstPasswordVal === secondPasswordVal && firstPasswordVal.length > 0) {
-        // If they match call checkPasswords to make sure it matches the rest of the requirements.
-        checkPasswords();
-    } else {
-        secondPasswordInputIssuesTracker.addIssues("Passwords must match!");
-    }
-
-    /*
-     * Retrieve validation message strings once all requirements are checked.
-     */
-    var firstPasswordInputIssues = firstPasswordInputIssuesTracker.retrieveIssues();
-    var secondPasswordInputIssues = secondPasswordInputIssuesTracker.retrieveIssues();
-
-    firstPasswordEl.setCustomValidity(firstPasswordInputIssues);
-    secondPasswordEl.setCustomValidity(secondPasswordInputIssues);
-};
 
 // Since autofocus doesn't work with bootstrap modals, I am using this code from Bootstrap JS to replace it
 // for autofocus.
 $('.signUp, .newEvent, .login').on('shown.bs.modal', function() {
-    $('#fname').focus();
-    $('#eName').focus();
-    $('#logEmail').focus();
+	$('#fname').focus();
+	$('#eName').focus();
+	$('#logEmail').focus();
 });
--- js/meetupEvent.js
+++ js/meetupEvent.js
@@ -1,41 +1,120 @@
 (function() {
-    "use strict";
-    var meetupEventApp = angular.module('meetupEventApp', ['firebase']);
+	"use strict";
+	var meetupEventApp = angular.module('meetupEventApp', ['firebase']);
 
-    meetupEventApp.controller('MeetupEventCtrl', ['$scope', '$firebaseArray',
-        function($scope, $firebaseArray) {
-            var ref = new Firebase("https://vivid-torch-762.firebaseio.com/");
-            $scope.eventsArray = $firebaseArray(ref);
-            $scope.master = {};
-            $scope.login = function(user) {
-                $scope.master = angular.copy(user);
-                if ($scope.master.email && $scope.master.pass) {
-                    ref.authWithPassword({
-                        email: $scope.master.email,
-                        password: $scope.master.pass
-                    }, function(error, authData) {
-                        if (error) {
-                            console.log("Login Failed!", error);
-                        } else {
-                            console.log("Authenticated successfully with payload:", authData);
-                            $('.login').modal('hide');
-                        }
-                    });
-                }
-            };
-            /*
+	meetupEventApp.controller('MeetupEventCtrl', ['$scope', '$firebaseArray',
+		function($scope, $firebaseArray) {
+			var ref = new Firebase("https://vivid-torch-762.firebaseio.com/");
+			//$scope.eventsArray = $firebaseArray(ref);
+			$scope.master = {};
+			$scope.masterUser = {};
+			$scope.login = function(user) {
+				$scope.master = angular.copy(user);
+				if ($scope.master.email && $scope.master.pass) {
+					ref.authWithPassword({
+						email: $scope.master.email,
+						password: $scope.master.pass
+					}, function(error, authData) {
+						if (error) {
+							console.log("Login Failed!", error);
+						} else {
+							console.log("Authenticated successfully with payload:", authData);
+							$('#loginForm')[0].reset();
+							$('.login').modal('hide');
+						}
+					});
+				}
+			};
+			$scope.signUp = function(newUser) {
+				$scope.masterUser = angular.copy(newUser);
+				// Create a callback to handle the result of the authentication
+				function authHandler(error, authData) {
+					if (error) {
+						console.log("Login Failed!", error);
+					} else {
+						console.log("Authenticated successfully with payload:", authData);
+						// save the user's profile into the database so we can list users,
+						// use them in Security and Firebase Rules, and show profiles
+						ref.child("users").child(authData.uid).set({
+							provider: authData.provider,
+							name: $scope.masterUser.fname
+						});
+					}
+				}
 
-                        ref.createUser({
-                            email: $scope.userEmail,
-                            password: $scope.userPass
-                        }, function(error, userData) {
-                            if (error) {
-                                console.log("Error creating user:", error);
-                            } else {
-                                console.log("Successfully created user account with uid:", userData.uid);
-                            }
-                        });
-            			*/
-        }
-    ]);
+				/*
+				 * Make an issue tracker for each password input to have two separate issue trackers.
+				 */
+				var firstPasswordInputIssuesTracker = new TrackIssues(),
+					secondPasswordInputIssuesTracker = new TrackIssues();
+
+				/*
+				 * Checks to make sure first password doesn't have any issues.
+				 */
+				function checkPasswords() {
+					if ($scope.masterUser.fPassword.length < 8) {
+						firstPasswordInputIssuesTracker.addIssues("fewer than 8 characters");
+					} else if ($scope.masterUser.fPassword.length > 50) {
+						firstPasswordInputIssuesTracker.addIssues("greater than 50 characters");
+					}
+
+					if (!$scope.masterUser.fPassword.match(/[\!\@\#\$\%\^\&\*]/g)) {
+						firstPasswordInputIssuesTracker.addIssues("missing a symbol (!, @, #, $, %, ^, &, *)");
+					}
+
+					if (!$scope.masterUser.fPassword.match(/\d/g)) {
+						firstPasswordInputIssuesTracker.addIssues("missing a number");
+					}
+
+					if (!$scope.masterUser.fPassword.match(/[a-z]/g)) {
+						firstPasswordInputIssuesTracker.addIssues("missing a lowercase letter");
+					}
+
+					if (!$scope.masterUser.fPassword.match(/[A-Z]/g)) {
+						firstPasswordInputIssuesTracker.addIssues("missing an uppercase letter");
+					}
+
+					var badCharacterGroup = $scope.masterUser.fPassword.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g);
+					if (badCharacterGroup) {
+						badCharacterGroup.forEach(function(badChar) {
+							firstPasswordInputIssuesTracker.addIssues("includes bad character: " + badChar);
+						});
+					}
+				}
+
+				if ($scope.masterUser.fPassword === $scope.masterUser.sPassword && $scope.masterUser.fPassword.length > 0) {
+					// If they match call checkPasswords to make sure it matches the rest of the requirements.
+					checkPasswords();
+				} else {
+					secondPasswordInputIssuesTracker.addIssues("Passwords must match!");
+				}
+				/*
+				 * Retrieve validation message strings once all requirements are checked.
+				 */
+				var firstPasswordInputIssues = firstPasswordInputIssuesTracker.retrieveIssues();
+				var secondPasswordInputIssues = secondPasswordInputIssuesTracker.retrieveIssues();
+				firstPasswordEl.setCustomValidity(firstPasswordInputIssues);
+				secondPasswordEl.setCustomValidity(secondPasswordInputIssues);
+				// If first or second password have no issues then create the user!
+				if (!firstPasswordInputIssues.length && !secondPasswordInputIssues.length) {
+					ref.createUser({
+						email: $scope.masterUser.email,
+						password: $scope.masterUser.fPassword
+					}, function(error, userData) {
+						if (error) {
+							console.log("Error creating user:", error);
+						} else {
+							console.log("Successfully created user account with uid:", userData.uid);
+							ref.authWithPassword({
+								email: $scope.masterUser.email,
+								password: $scope.masterUser.fPassword
+							}, authHandler);
+							$('#signUpForm')[0].reset();
+							$('.signUp').modal('hide');
+						}
+					});
+				}
+			};
+		}
+	]);
 })();
