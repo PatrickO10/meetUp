a2c2d52744484429f8359e996adde09aa0bae6ba
patricko10 <patrickorth10@gmail.com>
2016-02-01 22:48:19 -0600

    adds gitignore file and changes the second field for date to end date

--- js/all.js
+++ js/all.js
@@ -14,25 +14,25 @@
 (function(){
 	'use strict';
 
+	angular.module('app.event', []);
+})();
+(function(){
+	'use strict';
+
 	angular.module('app.login', ['firebase']);
 
 })();
 (function(){
 	'use strict';
 
-	angular.module('app.event', []);
+	angular.module('app.fbAuth', ['firebase']);
+
 })();
 (function(){
 	'use strict';
 
 	angular
 		.module('app.register', []);
-})();
-(function(){
-	'use strict';
-
-	angular.module('app.fbAuth', ['firebase']);
-
 })();
 (function() {
 	'use strict';
@@ -72,54 +72,6 @@
 			$('#eName').focus();
 			$('#logEmail').focus();
 		});
-
-	}
-})();
-
-(function() {
-	'use strict';
-
-	angular
-		.module('app.login')
-		.controller('LoginCtrl', LoginCtrl);
-
-	LoginCtrl.$inject = ['$scope', '$rootScope', 'authService'];
-
-	function LoginCtrl($scope, $rootScope, authService) {
-		var self = this;
-		self.masterUser = {};
-		$scope.loginError = false;
-		$scope.loginErrMsg = '';
-
-
-		self.login = function(user) {
-			self.masterUser = angular.copy(user);
-
-			authService.loginWithPwd(self.masterUser).then(function(authData) {
-				$scope.loginError = false;
-				$scope.loginErrMsg = '';
-				$('#loginForm')[0].reset();
-				$('.login').modal('hide');
-			}, function(error) {
-				$scope.loginError = true;
-				switch (error.code) {
-					case "EMAIL_TAKEN":
-						$scope.loginErrMsg = "Error: The specified email is taken";
-						break;
-					case "INVALID_EMAIL":
-						$scope.loginErrMsg = "Error: The email you entered is invalid";
-						break;
-					case "INVALID_PASSWORD":
-						$scope.loginErrMsg = "Error: The specified password is incorrect.";
-						break;
-					case "INVALID_USER":
-						$scope.loginErrMsg = "Error: The specified user does not exist.";
-						break;
-					default:
-						$scope.loginErrMsg = "Error: " + error.code;
-				}
-			});
-		};
 
 	}
 })();
@@ -173,6 +125,54 @@
 			$('#newEventForm')[0].reset();
 			$('.newEvent').modal('hide');
 		};
+	}
+})();
+
+(function() {
+	'use strict';
+
+	angular
+		.module('app.login')
+		.controller('LoginCtrl', LoginCtrl);
+
+	LoginCtrl.$inject = ['$scope', '$rootScope', 'authService'];
+
+	function LoginCtrl($scope, $rootScope, authService) {
+		var self = this;
+		self.masterUser = {};
+		$scope.loginError = false;
+		$scope.loginErrMsg = '';
+
+
+		self.login = function(user) {
+			self.masterUser = angular.copy(user);
+
+			authService.loginWithPwd(self.masterUser).then(function(authData) {
+				$scope.loginError = false;
+				$scope.loginErrMsg = '';
+				$('#loginForm')[0].reset();
+				$('.login').modal('hide');
+			}, function(error) {
+				$scope.loginError = true;
+				switch (error.code) {
+					case "EMAIL_TAKEN":
+						$scope.loginErrMsg = "Error: The specified email is taken";
+						break;
+					case "INVALID_EMAIL":
+						$scope.loginErrMsg = "Error: The email you entered is invalid";
+						break;
+					case "INVALID_PASSWORD":
+						$scope.loginErrMsg = "Error: The specified password is incorrect.";
+						break;
+					case "INVALID_USER":
+						$scope.loginErrMsg = "Error: The specified user does not exist.";
+						break;
+					default:
+						$scope.loginErrMsg = "Error: " + error.code;
+				}
+			});
+		};
+
 	}
 })();
 
