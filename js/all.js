1b6c84b39a37c3a1b6c2f60fa4bd90002232aa7b
patricko10 <patrickorth10@gmail.com>
2016-02-04 00:08:34 -0600

    adds new color scheme and container-flex

--- js/all.js
+++ js/all.js
@@ -9,12 +9,12 @@
 (function(){
 	'use strict';
 
-	angular.module('app.dashboard', ['firebase']);
+	angular.module('app.event', []);
 })();
 (function(){
 	'use strict';
 
-	angular.module('app.event', []);
+	angular.module('app.dashboard', ['firebase']);
 })();
 (function(){
 	'use strict';
@@ -34,48 +34,6 @@
 	angular
 		.module('app.register', []);
 })();
-(function() {
-	'use strict';
-
-	angular
-		.module('app.dashboard')
-		.controller('MainCtrl', MainCtrl);
-
-	MainCtrl.$inject = ['authService', '$firebaseArray'];
-
-	function MainCtrl(authService, $firebaseArray) {
-		var self = this;
-
-		self.logOut = function() {
-			authService.logOutUser();
-			self.loggedStatus = false;
-		};
-
-		authService.setOnAuth(authDataCallback);
-
-		// Callback to set user's events.
-		function authDataCallback(authData) {
-			if (authData) {
-				console.log("User " + authData.uid + " is logged in with " + authData.provider);
-				var userRef = authService.setEventRef(authData.uid);
-				self.eventsArray = $firebaseArray(userRef);
-				self.loggedStatus = true;
-			} else {
-				console.log("User is logged out");
-			}
-		}
-
-		// Since autofocus doesn't work with bootstrap modals, I am using this code from Bootstrap JS to replace it
-		// for autofocus.
-		$('.signUp, .newEvent, .login').on('shown.bs.modal', function() {
-			$('#fname').focus();
-			$('#eName').focus();
-			$('#logEmail').focus();
-		});
-
-	}
-})();
-
 (function() {
 	'use strict';
 
@@ -125,6 +83,48 @@
 			$('#newEventForm')[0].reset();
 			$('.newEvent').modal('hide');
 		};
+	}
+})();
+
+(function() {
+	'use strict';
+
+	angular
+		.module('app.dashboard')
+		.controller('MainCtrl', MainCtrl);
+
+	MainCtrl.$inject = ['authService', '$firebaseArray'];
+
+	function MainCtrl(authService, $firebaseArray) {
+		var self = this;
+
+		self.logOut = function() {
+			authService.logOutUser();
+			self.loggedStatus = false;
+		};
+
+		authService.setOnAuth(authDataCallback);
+
+		// Callback to set user's events.
+		function authDataCallback(authData) {
+			if (authData) {
+				console.log("User " + authData.uid + " is logged in with " + authData.provider);
+				var userRef = authService.setEventRef(authData.uid);
+				self.eventsArray = $firebaseArray(userRef);
+				self.loggedStatus = true;
+			} else {
+				console.log("User is logged out");
+			}
+		}
+
+		// Since autofocus doesn't work with bootstrap modals, I am using this code from Bootstrap JS to replace it
+		// for autofocus.
+		$('.signUp, .newEvent, .login').on('shown.bs.modal', function() {
+			$('#fname').focus();
+			$('#eName').focus();
+			$('#logEmail').focus();
+		});
+
 	}
 })();
 
